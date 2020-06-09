0edf4d7186ee9178ca1536f3394093fe9ea55521
Ted Pedersen <tedpedersen@me.com>
2020-06-09 15:26:52 -0700

    adding array for questions, function for building quiz

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 var qIntro = document.getElementById("qIntro");
 var qQuestionsContainer = document.getElementById("qQuestionsContainer");
 var qTimeContainer = document.getElementById("qTimeContainer");
+var quizQustionsContainer = document.getElementById("qQuestionsContainer");
 
 //start the quiz 
 document.getElementById("qStart").addEventListener("click", function startQuiz(){
     //hide the intro
     qIntro.classList.add("hide");
     //show the questons and timer
-    qQuestionsContainer.classList.remove("hide");
     qTimeContainer.classList.remove("hide");
+    buildQuiz();
 });
 
 //timer for quiz
@@ -58,3 +59,39 @@ var qQuestions = [
       correctAnswer: "d"
     }
   ];
+
+function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
+
+    // for each question...
+    qQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+        // variable to store the list of possible answers
+        const answers = [];
+
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
+
+            // ...add an HTML radio button
+            answers.push(
+            `<button type="button" class="btn btn-primary answer-button" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+            </button>`
+            );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+            `<h1 class="question"> ${currentQuestion.question} </h1>
+            <div class="btn-group-vertical"> ${answers.join('')} </div>`
+        );
+    }
+);
+
+
+// finally combine our output list into one string of HTML and put it on the page
+quizQustionsContainer.innerHTML = output.join('');
+}
