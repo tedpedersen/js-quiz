8a5b698d2e89254684a4f4431336970a098c48ac
Ted Pedersen <tedpedersen@me.com>
2020-06-11 16:23:00 -0700

    logic and data for quiz questions

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ var qIntro = document.getElementById("qIntro");
 var qQuestionsContainer = document.getElementById("qQuestionsContainer");
 var qTimeContainer = document.getElementById("qTimeContainer");
 var quizQustionsContainer = document.getElementById("qQuestionsContainer");
-
+var quizAnswersContainer = document.getElementById("qAnswersContainer");
+var qResultContainer = document.getElementById("qResult");
+var qContainer = document.getElementById("qContainer");
+var timeleft = 100;
 //start the quiz 
 document.getElementById("qStart").addEventListener("click", function startQuiz(){
     //hide the intro
     qIntro.classList.add("hide");
-    //show the questons and timer
+    //show the questions and timer
     qTimeContainer.classList.remove("hide");
-    buildQuiz();
-});
-
-//timer for quiz
-var timeleft = 100;
+    buildQuestion1();
+    //timer for quiz
 var quizTimer = setInterval(function(){
   //if the timer runs out, the user is alerted and the quiz reloads  
   if(timeleft <= 0){
@@ -27,71 +27,296 @@ var quizTimer = setInterval(function(){
   }
   timeleft -= 1;
 }, 1000);
+});
+
+//set up users score
+var userScore = 0;
 
 //quiz questions
-var qQuestions = [
-    {
-      question: "Who invented JavaScript?",
-      answers: {
-        a: "Douglas Crockford",
-        b: "Sheryl Sandberg",
-        c: "Brendan Eich"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which one of these is a JavaScript package manager?",
-      answers: {
-        a: "Node.js",
-        b: "TypeScript",
-        c: "npm"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Which tool can you use to ensure code quality?",
-      answers: {
-        a: "Angular",
-        b: "jQuery",
-        c: "RequireJS",
-        d: "ESLint"
-      },
-      correctAnswer: "d"
-    }
-  ];
-
-function buildQuiz(){
-    // variable to store the HTML output
-    const output = [];
-
-    // for each question...
-    qQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-
-        // variable to store the list of possible answers
-        const answers = [];
-
-        // and for each available answer...
-        for(letter in currentQuestion.answers){
-
-            // ...add an HTML radio button
-            answers.push(
-            `<button type="button" class="btn btn-primary answer-button" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-            </button>`
-            );
+questionObj = {
+    "question1": { 
+        question: "Commonly used data types do NOT include:",
+        answers: {
+          a: "strings",
+          b: "booleans",
+          c: "alerts",
+          d: "numbers"
         }
+      },
+      "question2": { 
+        question: "The condition of a if / else statement is enclosed with:",
+        answers: {
+          a: "quotes",
+          b: "curly brackets",
+          c: "parentesis",
+          d: "square brackets"
+        }
+      },
+      "question3": { 
+        question: "String values must be enlosed within ......... when being assigned to variables.",
+        answers: {
+          a: "commas",
+          b: "curly brackets",
+          c: "quotes",
+          d: "parentesis"
+        }
+      },
+      "question4": { 
+        question: "A very useful tool used development and debugging for printing content to the debugger is:",
+        answers: {
+            a: "Javascript",
+            b: "terminal/bash",
+            c: "for loops",
+            d: "console.log"
+        }
+      }
+   }
 
-        // add this question and its answers to the output
-        output.push(
-            `<h1 class="question"> ${currentQuestion.question} </h1>
-            <div class="btn-group-vertical"> ${answers.join('')} </div>`
-        );
-    }
-);
+//refs to questions
+var qQuestion1 = questionObj.question1.question;
+var qQuestion2 = questionObj.question2.question;
+var qQuestion3 = questionObj.question3.question;
+var qQuestion4 = questionObj.question4.question;
 
+var answer1 = questionObj.question1.answers.a;
+var answer2 = questionObj.question1.answers.b;
+var answer3 = questionObj.question1.answers.c;
+var answer4 = questionObj.question1.answers.d;
+var answer5 = questionObj.question2.answers.a;
+var answer6 = questionObj.question2.answers.b;
+var answer7 = questionObj.question2.answers.c;
+var answer8 = questionObj.question2.answers.d;
+var answer9 = questionObj.question3.answers.a;
+var answer10 = questionObj.question3.answers.b;
+var answer11 = questionObj.question3.answers.c;
+var answer12 = questionObj.question3.answers.d;
+var answer13 = questionObj.question4.answers.a;
+var answer14 = questionObj.question4.answers.b;
+var answer15 = questionObj.question4.answers.c;
+var answer16 = questionObj.question4.answers.d;
 
-// finally combine our output list into one string of HTML and put it on the page
-quizQustionsContainer.innerHTML = output.join('');
+// create questions
+var q1 = document.createElement("h1");
+q1.textContent = qQuestion1;
+var q2 = document.createElement("h1");
+q2.textContent = qQuestion2;
+var q3 = document.createElement("h1");
+q3.textContent = qQuestion3;
+var q4 = document.createElement("h1");
+q4.textContent = qQuestion4;
+
+//create answers
+var btnClass = "btn btn-primary answer-button";
+var a1 = document.createElement("button");
+a1.textContent = answer1;
+a1.className = btnClass;
+a1.setAttribute("value", "wrong");
+var a2 = document.createElement("button");
+a2.textContent = answer2;
+a2.className = btnClass;
+a2.setAttribute("value", "wrong");
+var a3 = document.createElement("button");
+a3.textContent = answer3;
+a3.className = btnClass;
+a3.setAttribute("value", "correct");
+var a4 = document.createElement("button");
+a4.textContent = answer4;
+a4.className = btnClass;
+a4.setAttribute("value", "wrong");
+var a5 = document.createElement("button");
+a5.textContent = answer5;
+a5.className = btnClass;
+a5.setAttribute("value", "wrong");
+var a6 = document.createElement("button");
+a6.textContent = answer6;
+a6.className = btnClass;
+a6.setAttribute("value", "wrong");
+var a7 = document.createElement("button");
+a7.textContent = answer7;
+a7.className = btnClass;
+a7.setAttribute("value", "correct");
+var a8 = document.createElement("button");
+a8.textContent = answer8;
+a8.className = btnClass;
+a8.setAttribute("value", "wrong");
+var a9 = document.createElement("button");
+a9.textContent = answer9;
+a9.className = btnClass;
+a9.setAttribute("value", "wrong");
+var a10 = document.createElement("button");
+a10.textContent = answer10;
+a10.className = btnClass;
+a10.setAttribute("value", "wrong");
+var a11 = document.createElement("button");
+a11.textContent = answer11;
+a11.className = btnClass;
+a11.setAttribute("value", "correct");
+var a12 = document.createElement("button");
+a12.textContent = answer12;
+a12.className = btnClass;
+a12.setAttribute("value", "wrong");
+var a13 = document.createElement("button");
+a13.textContent = answer13;
+a13.className = btnClass;
+a13.setAttribute("value", "wrong");
+var a14 = document.createElement("button");
+a14.textContent = answer14;
+a14.className = btnClass;
+a14.setAttribute("value", "wrong");
+var a15 = document.createElement("button");
+a15.textContent = answer15;
+a15.className = btnClass;
+a15.setAttribute("value", "wrong");
+var a16 = document.createElement("button");
+a16.textContent = answer16;
+a16.className = btnClass;
+a16.setAttribute("value", "correct");
+
+//build the question and answers
+function buildQuestion1(){
+    quizQustionsContainer.appendChild(q1);
+    quizAnswersContainer.appendChild(a1);
+    quizAnswersContainer.appendChild(a2);
+    quizAnswersContainer.appendChild(a3);
+    quizAnswersContainer.appendChild(a4);
+    getAnswer1();
 }
+
+function getAnswer1() { qContainer.addEventListener('click', (event) => {
+    if (event.target.matches(".answer-button")) {
+        var myValue = event.target.getAttribute("value");
+        console.log(myValue);
+        //check if answer is correct
+        if (myValue === "correct"){
+            qResultContainer.textContent = "CORRECT";
+            //give them points
+            userScore = userScore + 25;
+            console.log(userScore);
+
+           
+            setTimeout(() => { buildQuestion2(); }, 2000);
+        }
+        else{
+            qResultContainer.textContent = "WRONG";
+            console.log(userScore);
+            //reduce timer if wrong
+            timeleft = timeleft - 10;
+            //wait 2 secs until next questions
+            setTimeout(() => {  buildQuestion2(); }, 2000);
+
+        }
+      }
+  })
+}
+//clear the finished question
+function clearQuestion(){
+    quizQustionsContainer.innerHTML = '';
+    quizAnswersContainer.innerHTML = '';
+    qResultContainer.innerHTML = '';
+}
+function buildQuestion2(){
+    clearQuestion();
+    quizQustionsContainer.append(q2);
+    quizAnswersContainer.append(a5);
+    quizAnswersContainer.append(a6);
+    quizAnswersContainer.append(a7);
+    quizAnswersContainer.append(a8);
+    getAnswer2();
+}
+function getAnswer2() { qContainer.addEventListener('click', (event) => {
+    if (event.target.matches(".answer-button")) {
+        var myValue = event.target.getAttribute("value");
+        console.log(myValue);
+        if (myValue === "correct"){
+            qResultContainer.textContent = "CORRECT";
+            scoreCorrect();
+            setTimeout(() => { buildQuestion3(); }, 2000);
+        }
+        else{
+            qResultContainer.textContent = "WRONG";
+            timeleft = timeleft - 10;
+            scoreWrong();
+            setTimeout(() => {  buildQuestion3(); }, 2000);
+
+        }
+      }
+  })
+}
+
+function buildQuestion3(){
+    clearQuestion();
+    quizQustionsContainer.append(q3);
+    quizAnswersContainer.appendChild(a9);
+    quizAnswersContainer.appendChild(a10);
+    quizAnswersContainer.appendChild(a11);
+    quizAnswersContainer.appendChild(a12);
+    getAnswer3();
+}
+
+function getAnswer3() { qContainer.addEventListener('click', (event) => {
+    if (event.target.matches(".answer-button")) {
+        var myValue = event.target.getAttribute("value");
+        console.log(myValue);
+        if (myValue === "correct"){
+            qResultContainer.textContent = "CORRECT";
+            scoreCorrect();
+            setTimeout(() => { buildQuestion4(); }, 2000);
+        }
+        else{
+            qResultContainer.textContent = "WRONG";
+            timeleft = timeleft - 10;
+            scoreWrong();
+            setTimeout(() => {  buildQuestion4(); }, 2000);
+
+        }
+      }
+  })
+}
+
+function buildQuestion4(){
+    clearQuestion();
+    quizQustionsContainer.append(q4);
+    quizAnswersContainer.appendChild(a13);
+    quizAnswersContainer.appendChild(a14);
+    quizAnswersContainer.appendChild(a15);
+    quizAnswersContainer.appendChild(a16);
+    getAnswer4();
+}
+
+
+function getAnswer4() { qContainer.addEventListener('click', (event) => {
+    if (event.target.matches(".answer-button")) {
+        var myValue = event.target.getAttribute("value");
+        console.log(myValue);
+        if (myValue === "correct"){
+            qResultContainer.textContent = "CORRECT";
+            scoreCorrect();
+        }
+        else{
+            qResultContainer.textContent = "WRONG";
+            timeleft = timeleft - 10;
+            scoreWrong();
+        }
+      }
+  })
+}
+//logic for the scoring
+function scoreCorrect() {
+    userScore + 25;
+    console.log(userScore);
+}
+function scoreWrong() {
+    console.log(userScore);
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
